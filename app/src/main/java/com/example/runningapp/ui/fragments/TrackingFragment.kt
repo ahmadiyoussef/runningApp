12e9273b063b449764b3145f9abffd91a275eb53
S2M <ahmadiyoussef14@gmail.com>
2023-04-23 21:05:40 +0100

    setting up service

--- app/src/main/java/com/example/runningapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/runningapp/ui/fragments/TrackingFragment.kt
@@ -1,13 +1,70 @@
 package com.example.runningapp.ui.fragments
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.runningapp.R
+import com.example.runningapp.others.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.example.runningapp.ui.viewModels.MainViewModel
+import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_tracking.btnToggleRun
+import kotlinx.android.synthetic.main.fragment_tracking.mapView
 
 @AndroidEntryPoint
 class TrackingFragment: Fragment(R.layout.fragment_tracking) {
 
     private val viewModel: MainViewModel by viewModels()
+    private var map : GoogleMap? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync{
+            map = it
+        }
+        btnToggleRun.setOnClickListener {
+            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+        }
+
+
+    }
+
+    private fun sendCommandToService(action: String) =
+        Intent(requireContext(), TrackingFragment::class.java).also {
+             it.action = action
+            requireContext().startService(it)
+        }
+
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+    }
+
 }
